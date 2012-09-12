f3a20eaa88d360dfa60452ba39b3960ac91863f6
francisco.franciscotbjr@gmail.com <francisco.franciscotbjr@gmail.com@e2ab5a19-3bba-85ed-96de-cba4b1b7711c>
2012-09-12 04:04:20 +0000

    Persistence improvement implementations.

--- trunk/Yacamim/src/br/org/yacamim/persistence/PK.java
+++ trunk/Yacamim/src/br/org/yacamim/persistence/PK.java
@@ -18,11 +18,7 @@
  */
 package br.org.yacamim.persistence;
 
-import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
-import static java.lang.annotation.ElementType.CONSTRUCTOR;
-import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.METHOD;
-import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Documented;
@@ -37,7 +33,7 @@ import java.lang.annotation.Target;
  * @version 1.0
  * @since 1.0
  */
-@Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER })
+@Target({ METHOD})
 @Retention(RUNTIME)
 @Documented
 public @interface PK {
