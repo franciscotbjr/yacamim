3673228c949ea75ac0cb820fc234eff568cb5405
verissimo.manoel@gmail.com <verissimo.manoel@gmail.com@e2ab5a19-3bba-85ed-96de-cba4b1b7711c>
2013-02-17 19:42:02 +0000

    Ajustes no caso de uso de pacientes.

--- trunk/Yacamim_Samples/RadiografiaSocial/src/br/com/nuceloesperanca/radiografiasocial/PacienteActivity.java
+++ trunk/Yacamim_Samples/RadiografiaSocial/src/br/com/nuceloesperanca/radiografiasocial/PacienteActivity.java
@@ -5,18 +5,31 @@
  */
 package br.com.nuceloesperanca.radiografiasocial;
 
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import br.com.nuceloesperanca.radiografiasocial.entidade.Cid;
+import br.com.nuceloesperanca.radiografiasocial.entidade.Paciente;
 import br.com.nuceloesperanca.radiografiasocial.persitencia.CidDBAdapter;
+import br.com.nuceloesperanca.radiografiasocial.persitencia.PacienteDBAdapter;
+import br.com.nuceloesperanca.radiografiasocial.util.Constantes;
 import br.com.nuceloesperanca.radiografiasocial.util.UtilCombos;
 import br.org.yacamim.YBaseActivity;
+import br.org.yacamim.ui.components.BaseDatePickerDialog;
+import br.org.yacamim.ui.components.BaseOnDateSetListener;
+import br.org.yacamim.util.YUtilDate;
+import br.org.yacamim.util.YUtilString;
+import br.org.yacamim.util.YUtilText;
 
 /**
  * @author manny.
@@ -26,6 +39,9 @@ import br.org.yacamim.YBaseActivity;
  */
 public class PacienteActivity extends YBaseActivity {
 	private static final String TAG_CLASS = ConsultarPacienteActivity.class.getName();
+	static final int DATE_DIALOG_DATA_NASCIMENTO = 0;
+	private boolean inclusao = true;
+	private Paciente paciente = null;
 
 	/**
 	 * @see android.app.Activity#onCreate(android.os.Bundle)
@@ -52,13 +68,20 @@ public class PacienteActivity extends YBaseActivity {
      */
     private void init() {
     	try {
+    		if (getIntent().getExtras() == null || getIntent().getExtras().get("pacienteCadastro") == null) {
+    			this.paciente = new Paciente();
+    		} else {
+    			this.paciente = (Paciente) getIntent().getExtras().getParcelable("pacienteCadastro");
+    			inclusao = false;
+    		}
+
     		super.initGPS();
 
     		this.initCampos();
 
     		this.initCombos();
 
-    		//this.initDatePickers();
+    		this.initDatePickers();
 		} catch (Exception e) {
 			Log.e(TAG_CLASS, e.getMessage());
 		}
@@ -96,6 +119,18 @@ public class PacienteActivity extends YBaseActivity {
 		}
     }
 
+    /**
+     * Inicializa os componentes de data. 
+     */
+    private void initDatePickers() {
+    	final Button pickDataInstalacao = (Button) findViewById(R.id.btn_nacimento);
+        pickDataInstalacao.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                showDialog(DATE_DIALOG_DATA_NASCIMENTO);
+            }
+        });
+    }
+
 	/**
      * Salva os dados do paciente.
      *  
@@ -103,9 +138,112 @@ public class PacienteActivity extends YBaseActivity {
      */
     public void salvar(final View view) {
     	try {
-
+    		if (this.realizaValidacoes()) {
+    			gravarDados();
+    		}
 		} catch (Exception e) {
 			Log.e(TAG_CLASS, e.getMessage());
+		}
+    }
+
+    /**
+     * Realiza as validações do formulário.
+     * 
+     * @return
+     */
+    private boolean realizaValidacoes() {
+    	List<String> nomesCampos = new ArrayList<String>();
+    	if (!this.validaCamposObrigatorios(nomesCampos)) {
+    		this.buildMessage(nomesCampos, R.string.msg1);
+    		this.showDialog(Constantes.ERROR_REQUIRED_FIELDS);
+    		return false;
+    	}
+
+    	return true;
+    }
+
+    /**
+	 * Realiza a gravação dos dados do paciente. 
+	 */
+    private void gravarDados() {
+    	PacienteDBAdapter pacienteDBAdapter = null;
+    	try {
+			pacienteDBAdapter = new PacienteDBAdapter(Paciente.class);
+			pacienteDBAdapter.open();
+			pacienteDBAdapter.beginTransaction();
+			paciente.setProntuario(YUtilText.getIntegerFromTextView(this, R.id.txte_prontuario));
+			paciente.setNome(YUtilText.getTextFromEditText(this, R.id.txte_nome));
+			paciente.setNascimento((YUtilDate.convertDate(YUtilText.getTextFromTextView(this, R.id.txtv_nascimento))));
+			paciente.setNomeMae(YUtilText.getTextFromEditText(this, R.id.txte_nome_mae));
+			paciente.setNomePai(YUtilText.getTextFromEditText(this, R.id.txte_nome_pai));
+			paciente.setIrmaos(YUtilText.getTextFromEditText(this, R.id.txte_irmaos));
+			paciente.setTelefones(YUtilText.getTextFromEditText(this, R.id.txte_telefones));
+			paciente.setEndereco(YUtilText.getTextFromEditText(this, R.id.txte_endereco));
+
+			if (((Spinner) this.findViewById(R.id.cmb_cid)).getSelectedItem() != null) {
+				paciente.setCid((Cid)((Spinner)this.findViewById(R.id.cmb_cid)).getSelectedItem());
+			}
+
+			if (YUtilText.getLongFromEditText(this, R.id.txte_latitude) != null) {
+				paciente.setLatitude(YUtilText.getLongFromEditText(this, R.id.txte_latitude));
+			}
+
+			if (YUtilText.getLongFromEditText(this, R.id.txte_longitude) != null) {
+				paciente.setLongitude(YUtilText.getLongFromEditText(this, R.id.txte_longitude));
+			}
+
+			if (inclusao) {
+				pacienteDBAdapter.insert(paciente);
+				showDialog(Constantes.INFO_DATA_SUCCESFULLY_INSERTED);
+			} else {
+				pacienteDBAdapter.update(paciente);
+				showDialog(Constantes.INFO_DATA_SUCCESFULLY_UPDATED);
+			}
+		} catch (Exception e) {
+			Log.e(TAG_CLASS, e.getMessage());
+			if (pacienteDBAdapter != null) {
+				pacienteDBAdapter.endTransaction(false);
+				pacienteDBAdapter.close();
+			}
+		} finally {
+			if (pacienteDBAdapter != null) {
+				pacienteDBAdapter.endTransaction(true);
+				pacienteDBAdapter.close();
+			}
+		}
+    }
+
+    /**
+     * Valida se os campos obrigatórios foram preenchidos.
+     * 
+     * @param nomesCampos
+     * @return
+     */
+    private boolean validaCamposObrigatorios(List<String> nomesCampos) {
+    	if (YUtilString.isEmptyString(YUtilText.getTextFromEditText(this, R.id.txte_nome))) {
+        	nomesCampos.add(this.getText(R.string.lbl_nome).toString());
+    	}
+		return nomesCampos.size() == 0;
+	}
+
+    /**
+     * @see br.com.poliedro.arquitetura.android.PopupMenuBaseActivity#onCreateDialog(int)
+     */
+    @Override
+    protected Dialog onCreateDialog(final int idDialog) {
+    	final Calendar calendarInicio = YUtilDate.getCalendar(super.getBestLocalization());
+    	switch (idDialog) {
+			case DATE_DIALOG_DATA_NASCIMENTO:
+	            return new BaseDatePickerDialog(
+	            		this, 
+	            		new BaseOnDateSetListener((TextView) findViewById(R.id.txtv_nascimento), "dd/MM/yyyy"), 
+	            		calendarInicio.get(Calendar.YEAR), 
+	            		calendarInicio.get(Calendar.MONTH), 
+	            		calendarInicio.get(Calendar.DAY_OF_MONTH), 
+	            		R.string.valor_definir_data,
+	            		true);
+			default:
+				return super.onCreateDialog(idDialog);
 		}
     }
 }
