f8031688ce6cab942018d9937c3b29ac9d5d6b47
francisco.franciscotbjr@gmail.com <francisco.franciscotbjr@gmail.com@e2ab5a19-3bba-85ed-96de-cba4b1b7711c>
2013-02-20 23:08:36 +0000

    ManyToMany relationship

---
+++ Yacamim_Samples/YacamimRelationshipMappingDefaults/src/br/org/yacamim/relationship/mapping/defaults/unidirectionalMultiValuedRelationships/OneToManyActivity.java
@@ -0,0 +1,48 @@
+/**
+ * OneToManyActivity.java
+ *
+ * Copyright 2013 yacamim.org.br
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package br.org.yacamim.relationship.mapping.defaults.unidirectionalMultiValuedRelationships;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import br.org.yacamim.relationship.mapping.defaults.R;
+
+/**
+ * Classe OneToManyActivity TODO
+ *
+ * @author yacamim.org.br (Francisco Tarcizo Bomfim Júnior)
+ * @version 1.0
+ * @since 1.0
+ */
+public class OneToManyActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_unidirectional_one_to_many);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_unidirectional_one_to_many,
+				menu);
+		return true;
+	}
+
+}
