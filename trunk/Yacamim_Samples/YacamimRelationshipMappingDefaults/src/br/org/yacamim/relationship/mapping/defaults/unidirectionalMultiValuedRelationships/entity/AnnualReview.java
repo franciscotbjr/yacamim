e6ae8eb83d34ba5d6922622412e8c47871775adb
francisco.franciscotbjr@gmail.com <francisco.franciscotbjr@gmail.com@e2ab5a19-3bba-85ed-96de-cba4b1b7711c>
2013-02-28 01:00:52 +0000

    Unidirectional Multi-Valued Relationships

---
+++ trunk/Yacamim_Samples/YacamimRelationshipMappingDefaults/src/br/org/yacamim/relationship/mapping/defaults/unidirectionalMultiValuedRelationships/entity/AnnualReview.java
@@ -0,0 +1,139 @@
+/**
+ * AnnualReview.java
+ *
+ * Copyright 2013 yacamim.org.br
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package br.org.yacamim.relationship.mapping.defaults.unidirectionalMultiValuedRelationships.entity;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+import br.org.yacamim.entity.YBaseEntity;
+import br.org.yacamim.persistence.Column;
+import br.org.yacamim.persistence.Entity;
+import br.org.yacamim.persistence.Id;
+import br.org.yacamim.persistence.Table;
+
+/**
+ * Classe AnnualReview TODO
+ *
+ * @author yacamim.org.br (Francisco Tarcizo Bomfim Júnior)
+ * @version 1.0
+ * @since 1.0
+ */
+@Entity
+@Table(name="AnnualReview_umvr")
+public class AnnualReview extends YBaseEntity {
+	
+	private long id;
+	private String description;
+
+	/**
+	 * 
+	 */
+	public AnnualReview() {
+		super();
+	}
+
+	/**
+	 * @param parcel
+	 */
+	public AnnualReview(final Parcel parcel) {
+		super(parcel);
+	}
+
+	/**
+	 * 
+	 */
+	@Id
+	public long getId() {
+		return id;
+	}
+
+	/**
+	 * 
+	 */
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	@Column
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * 
+	 * @param description
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+	/**
+	 * 
+	 */
+	public static final Parcelable.Creator<AnnualReview> CREATOR = new Parcelable.Creator<AnnualReview>() {
+        public AnnualReview createFromParcel(Parcel pc) {
+            return new AnnualReview(pc);
+        }
+        public AnnualReview[] newArray(int size) {
+            return new AnnualReview[size];
+        }
+	};
+
+	/**
+	 * 
+	 * @see br.org.yacamim.entity.YBaseEntity#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result
+				+ ((description == null) ? 0 : description.hashCode());
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	/**
+	 * 
+	 * @see br.org.yacamim.entity.YBaseEntity#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AnnualReview other = (AnnualReview) obj;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+
+}
