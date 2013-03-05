eef0cefa814b2287dc4fe76b6a2a7b526ca0b4b9
francisco.franciscotbjr@gmail.com <francisco.franciscotbjr@gmail.com@e2ab5a19-3bba-85ed-96de-cba4b1b7711c>
2013-03-05 01:34:19 +0000

    Fragments

---
+++ trunk/Yacamim/src/br/org/yacamim/YBaseTabFragmentActivity.java
@@ -0,0 +1,73 @@
+/**
+ * YBaseTabFragmentActivity.java
+ * 
+ * Copyright 2013 yacamim.org.br
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package br.org.yacamim;
+
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.FragmentTransaction;
+
+
+/**
+ * 
+ * Class YBaseTabFragmentActivity TODO
+ * 
+ * @author yacamim.org.br (Francisco Tarcizo Bomfim Júnior)
+ * @version 1.0
+ * @since 1.0
+ */
+public abstract class YBaseTabFragmentActivity extends YBaseFragmentActivity implements ActionBar.TabListener  {
+
+	/**
+	 * 
+	 */
+	public YBaseTabFragmentActivity() {
+		super();
+		YacamimState.getInstance().setCurrentActivity(this);
+	}
+
+	/**
+	 * 
+	 * @see android.app.ActionBar.TabListener#onTabReselected(android.app.ActionBar.Tab, android.app.FragmentTransaction)
+	 */
+	@Override
+	public void onTabReselected(Tab tab, FragmentTransaction ft) {
+		
+		
+	}
+
+	/**
+	 * 
+	 * @see android.app.ActionBar.TabListener#onTabSelected(android.app.ActionBar.Tab, android.app.FragmentTransaction)
+	 */
+	@Override
+	public void onTabSelected(Tab tab, FragmentTransaction ft) {
+		
+		
+	}
+
+	/**
+	 * 
+	 * @see android.app.ActionBar.TabListener#onTabUnselected(android.app.ActionBar.Tab, android.app.FragmentTransaction)
+	 */
+	@Override
+	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+		
+		
+	}
+
+}
